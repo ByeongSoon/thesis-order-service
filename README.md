494a7a91ea9606170cf1c6644bf8fda18eae6fe5
Byeongsoon Jang <byeongsoon@wisoft.io>
2021-08-05 23:11:34 +0900

    Add Order Microservice

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # thesis-order-service
+
 석사학위 논문구현 - Order Service 소스코드
---
+++ src/main/java/demo/orderservice/controller/TestController.java
@@ -0,0 +1,16 @@
+package demo.orderservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/order")
+public class TestController {
+
+  @GetMapping("")
+  public String getOrderTest() {
+    return "Order Service!!";
+  }
+
+}
