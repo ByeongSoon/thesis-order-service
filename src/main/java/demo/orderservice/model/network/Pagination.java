ffc2e04a5cb0b768a50fccc75aac97dbffa73c61
Byeongsoon Jang <byeongsoon94@gmail.com>
2021-08-23 17:53:30 +0900

    Add ItemApiController, ItemApiLogicService

--- src/main/java/demo/orderservice/model/network/Pagination.java
+++ src/main/java/demo/orderservice/model/network/Pagination.java
@@ -1,5 +1,14 @@
 package demo.orderservice.model.network;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Pagination {
 
     private Integer totalPage;
