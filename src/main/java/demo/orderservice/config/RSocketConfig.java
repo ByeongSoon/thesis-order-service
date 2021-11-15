e7877d7386382cb5eeece68c7e2d6e0917e51af9
Byeongsoon Jang <byeongsoon94@gmail.com>
2021-11-15 23:27:05 +0900

    RSocket Config 수정

--- src/main/java/demo/orderservice/config/RSocketConfig.java
+++ src/main/java/demo/orderservice/config/RSocketConfig.java
@@ -1,18 +1,29 @@
 package demo.orderservice.config;
 
+import io.rsocket.RSocket;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.cbor.Jackson2CborDecoder;
+import org.springframework.http.codec.cbor.Jackson2CborEncoder;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.messaging.rsocket.RSocketStrategies;
+
+import java.time.Duration;
 
 @Configuration
 public class RSocketConfig {
 
   @Bean
   RSocketRequester rSocketRequester(RSocketStrategies strategies) {
+    strategies = RSocketStrategies.builder()
+        .encoders(encoders -> encoders.add(new Jackson2CborEncoder()))
+        .decoders(decoders -> decoders.add(new Jackson2CborDecoder()))
+        .build();
+
     return RSocketRequester.builder()
         .rsocketStrategies(strategies)
-        .tcp("localhost",7000);
+        .connectTcp("localhost",7000)
+        .block(Duration.ofSeconds(5));
   }
 
 }
