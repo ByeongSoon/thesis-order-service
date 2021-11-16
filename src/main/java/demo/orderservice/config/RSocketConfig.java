2597486f992156cfd47b3faa15ff2d36b3e35657
Byeongsoon Jang <byeongsoon94@gmail.com>
2021-11-16 17:31:31 +0900

    RSocketConfig 수정

--- src/main/java/demo/orderservice/config/RSocketConfig.java
+++ src/main/java/demo/orderservice/config/RSocketConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.codec.cbor.Jackson2CborDecoder;
 import org.springframework.http.codec.cbor.Jackson2CborEncoder;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.messaging.rsocket.RSocketStrategies;
 
@@ -16,14 +18,20 @@ public class RSocketConfig {
   @Bean
   RSocketRequester rSocketRequester(RSocketStrategies strategies) {
     strategies = RSocketStrategies.builder()
-        .encoders(encoders -> encoders.add(new Jackson2CborEncoder()))
-        .decoders(decoders -> decoders.add(new Jackson2CborDecoder()))
+        .encoders(encoders -> encoders.add(new Jackson2JsonEncoder()))
+        .decoders(decoders -> decoders.add(new Jackson2JsonDecoder()))
         .build();
 
-    return RSocketRequester.builder()
+    return  RSocketRequester.builder()
         .rsocketStrategies(strategies)
-        .connectTcp("localhost",7000)
-        .block(Duration.ofSeconds(5));
+        .rsocketConnector(connector -> connector.keepAlive(Duration.ofSeconds(100),Duration.ofSeconds(5000)))
+        .tcp("localhost",7000);
+//        .connectTcp("localhost",7000)
+//        .retry()
+//        .block();
+//        .retry()
+//        .cache()
+//        .block(Duration.ofSeconds(5));
   }
 
 }
